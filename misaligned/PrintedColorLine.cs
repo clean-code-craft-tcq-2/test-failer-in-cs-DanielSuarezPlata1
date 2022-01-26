61d12e2f6dbef96666f9a6fc2e39d538e3573555
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-01-25 21:18:16 -0600

    Alignment and Colors Printed Correctly tests added

---
+++ misaligned/PrintedColorLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace misaligned
+{
+    public class PrintedColorLine
+    {
+        public string majorColor { get; set; }
+        public string minorColor { get; set; }
+        public string printedLine { get; set; }
+    }
+}
--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,23 +1,67 @@
-﻿using System;
+﻿using misaligned;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
-        static int printColorMap() {
+        static List<PrintedColorLine> printColorMap() {
+
+            List<PrintedColorLine> printedColorLines = new List<PrintedColorLine>();
+
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+
+                    string lineToPrint = String.Format("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+
+                    printedColorLines.Add(new PrintedColorLine { majorColor = majorColors[i], minorColor = minorColors[i],  printedLine =lineToPrint });
+
+                    Console.WriteLine(lineToPrint);
                 }
             }
-            return i * j;
+            return printedColorLines;
         }
         static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+            List<PrintedColorLine> result = printColorMap();
+
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+            int numberOfPairs = majorColors.Length * minorColors.Length;
+
+            Debug.Assert(result.Count == numberOfPairs);
+
+            //Check if the alignment is correct
+            Debug.Assert(result[0].printedLine.Length == result[numberOfPairs-1].printedLine.Length);
+
+            Debug.Assert(colorsPrintedCorrectly(result));
+
             Console.WriteLine("All is well (maybe!)");
+        }
+
+        static bool colorsPrintedCorrectly(List<PrintedColorLine> printedColorLines)
+        {
+            string latestMinorColor = "";
+
+            bool isCorrectlyPrinted = true;
+
+            for (int i = 0; i < printedColorLines.Count; i++)
+            {
+                if (latestMinorColor == printedColorLines[i].minorColor)
+                {
+                    isCorrectlyPrinted = false;
+
+                    break;
+                }
+
+                latestMinorColor = printedColorLines[i].minorColor;
+
+            }
+
+            return isCorrectlyPrinted;
         }
     }
 }
